371f30d6d7194aaa35be0fa859b7d5f63729f097
Luke Anderson <lukeqanderson@gmail.com>
2022-01-02 00:55:32 +0900

    set up cards to be automatically added to cardList which is rendered to the user

--- src/index.js
+++ src/index.js
@@ -1,26 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
+import CardList from './CardList';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import { friends } from './friends';
 
 ReactDOM.render(
   <React.StrictMode>
-    <div>
-      <Card image ={friends[0].image} name={friends[0].name} email={friends[0].email} phone={friends[0].phone}/>  
-      <Card image ={friends[1].image} name={friends[1].name} email={friends[1].email} phone={friends[1].phone}/>  
-      <Card image ={friends[2].image} name={friends[2].name} email={friends[2].email} phone={friends[2].phone}/>  
-      <Card image ={friends[3].image} name={friends[3].name} email={friends[3].email} phone={friends[3].phone}/>  
-      <Card image ={friends[4].image} name={friends[4].name} email={friends[4].email} phone={friends[4].phone}/>  
-      <Card image ={friends[5].image} name={friends[5].name} email={friends[5].email} phone={friends[5].phone}/>  
-      <Card image ={friends[6].image} name={friends[6].name} email={friends[6].email} phone={friends[6].phone}/>  
-      <Card image ={friends[7].image} name={friends[7].name} email={friends[7].email} phone={friends[7].phone}/>  
-      <Card image ={friends[8].image} name={friends[8].name} email={friends[8].email} phone={friends[8].phone}/>  
-      <Card image ={friends[9].image} name={friends[9].name} email={friends[9].email} phone={friends[9].phone}/>  
-      <Card image ={friends[10].image} name={friends[10].name} email={friends[10].email} phone={friends[10].phone}/>  
-    </div>    
+    <CardList friends={friends}/> 
   </React.StrictMode>,
   document.getElementById('root')
 );
